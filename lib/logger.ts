223ba5f6a937435a7e6b66406850cc42de14360d
Bayangmbe Mounmo <bayangp0@gmail.com>
2023-10-05 11:47:28 +0200

    initial commit for the speed startup of nextron app

--- lib/logger.ts
+++
@@ -1,9 +0,0 @@
-import chalk from 'chalk'
-
-export const info = (text: string) => {
-  console.log(chalk`{cyan [nextron]} ${text}`)
-}
-
-export const error = (message: string) => {
-  console.log(chalk`{cyan [nextron]} {red ${message}}`)
-}
